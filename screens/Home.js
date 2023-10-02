c2b2dec9a3bf22e8e1de6cf036c953976d26816c
jnip2 <jnip2@my.bcit.ca>
2023-10-02 12:53:49 -0700

    add basic light/dark theme bg and text color

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Appearance, useColorScheme } from 'react-native';
+import { useState } from 'react';
 
-export default function Home({navigation}) {
+export default function Home({ navigation }) {
+    const colorScheme = useColorScheme();
+
+    const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+    const themeContainerStyle =
+        colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
     return (
-        <View style={styles.container}>
-            <Text>Moneyment</Text>
+        <View style={[styles.container, themeContainerStyle]}>
+            <Text style={[styles.text, themeTextStyle]}>Your current theme is: {colorScheme}</Text>
             <StatusBar style="auto" />
-            <Button 
+            {/* <Button
                 title="Go to about page"
                 onPress={() => navigation.push('About')}
-            />
+            /> */}
         </View>
     );
 }
@@ -17,8 +25,20 @@ export default function Home({navigation}) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    lightContainer: {
+        backgroundColor: '#d0d0c0',
+    },
+    darkContainer: {
+        backgroundColor: '#242c40',
+    },
+    lightThemeText: {
+        color: '#242c40',
+    },
+    darkThemeText: {
+        color: '#d0d0c0',
+    },
 });
+
