a9a5e4403b77e77271aed972267592be577d8085
Corey J Rutt <cjw.rutt@gmail.com>
2023-10-12 14:55:13 -0700

    Updated to not require imports at the top for images

--- components/atoms/IconContainer/index.js
+++ components/atoms/IconContainer/index.js
@@ -2,18 +2,17 @@ import { View, StyleSheet } from "react-native"
 import { Image } from "expo-image"
 import { Iconify } from 'react-native-iconify';
 
-import food from "../../../assets/icons/fruits/Food.png"
-
 export default function IconContainer({ icon, size, colour }) {
+    const imageSources = {
+        settings: <Iconify icon='uil:setting' size={size} color={colour} />,
+        calendar: <Iconify icon='uil:schedule' size={size} color={colour} />,
+        food: <Image source={require("../../../assets/icons/fruits/Food.png")} alt='' style={styles.xs} contentFit="contain" />,
+    };
+
     return (
-        <>
-            <View style={styles.container} >
-                {icon === 'settings' ? <Iconify icon='uil:setting' size={size} color={colour} />
-                    : icon === 'calendar' ? <Iconify icon='uil:schedule' size={size} color={colour} />
-                        : icon === 'food' ? <Image source={food} alt='' style={styles.xs} contentFit="contain" />
-                            : <></>}
-            </View>
-        </>
+        <View style={styles.container}>
+            {imageSources[icon] || <></>}
+        </View>
     )
 }
 
