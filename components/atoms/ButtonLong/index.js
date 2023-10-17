b7526ab677068ccc3f0f9efe0a2ba6c9f57c69c9
jnip2 <jnip2@my.bcit.ca>
2023-10-17 12:20:12 -0700

    change name to differentiate from other buttons

---
+++ components/atoms/ButtonLong/index.js
@@ -0,0 +1,47 @@
+import { Text, Pressable, StyleSheet } from "react-native"
+
+// Returns Pressable element (basically a button that allows for stylizing) depending on text prop (string)
+
+export default function ButtonLong({ text }) {
+    return (
+        <>
+            {text && text === 'view' ? <Pressable style={[styles.button, styles.black]}>
+                <Text style={styles.text}>View Transactions</Text>
+            </Pressable> :
+                text === 'saveBudget' ? <Pressable style={[styles.button, styles.orange]}>
+                    <Text style={styles.text}>Save</Text>
+                </Pressable> :
+                    text === 'saveGoal' ? <Pressable style={[styles.button, styles.green]}>
+                        <Text style={styles.text}>Save</Text>
+                    </Pressable> : <></>}
+        </>
+    )
+}
+
+styles = StyleSheet.create({
+    button: {
+        height: 43,
+        width: 356,
+        justifyContent: 'center',
+        alignContent: 'center',
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -1 },
+        shadowOpacity: 0.25,
+        shadowRadius: 6
+    },
+    text: {
+        fontSize: 16,
+        textAlign: 'center',
+        color: '#ffffff',
+    },
+    black: {
+        backgroundColor: 'black',
+    },
+    orange: {
+        backgroundColor: '#E58331'
+    },
+    green: {
+        backgroundColor: '#508C46'
+    }
+})
