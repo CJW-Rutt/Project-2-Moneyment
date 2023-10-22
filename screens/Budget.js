6bde8297f88e140869371ef67c557c3850de520a
jnip2 <jnip2@my.bcit.ca>
2023-10-22 13:06:48 -0700

    add BudgetSingleTemplate

--- screens/Budget.js
+++ screens/Budget.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Appearance, useColorScheme } from 'react-native';
 import NavButton from '../components/atoms/NavButton';
+import BudgetSingleTemplate from '../components/templates/Budget/BudgetSingleTemplate';
+import { useState } from 'react';
 
 export default function Budget({navigation}) {
     const colorScheme = useColorScheme();
@@ -11,10 +13,13 @@ export default function Budget({navigation}) {
     const themeContainerStyle =
         colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
 
+    const [visible, setVisible] = useState(false)
+
     return (
         <View style={[styles.container, themeContainerStyle]} >
-            <Text style={[styles.text, themeTextStyle]} >Budget TIME!!</Text>
-            <NavButton icon="budget" active  />
+            {/* <Text style={[styles.text, themeTextStyle]} >Budget TIME!!</Text> */}
+            {/* <NavButton icon="budget" active  /> */}
+            <BudgetSingleTemplate />
             <StatusBar style="auto" />
         </View>
     );
