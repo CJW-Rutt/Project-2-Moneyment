cfc04f258c4abc0505fd012e1efa0af267a3dd42
justinweiyungwu <justinweiyungwu@gmail.com>
2023-10-31 09:42:26 -0700

    updated stylizing bugs for budgetdropdown ios and android.
    justin wu tues oct 31

--- components/atoms/BudgetDropdown/index.js
+++ components/atoms/BudgetDropdown/index.js
@@ -5,42 +5,79 @@ import { View, Text, TouchableOpacity, Modal, StyleSheet, ScrollView } from 'rea
 import { Picker } from "@react-native-picker/picker"
 
 const DualDropdownMenu = () => {
-  const [selectedOption1, setSelectedOption1] = useState('Week');
-  const [selectedOption2, setSelectedOption2] = useState('Next Week');
+  const [selectedItem1, setSelectedItem1] = useState("Day");
+  const [selectedItem2, setSelectedItem2] = useState('Next Week');
 
-  const options1 = ['Day', 'Week', "2 Weeks", "Month"];
-  const options2 = ['Today', 'Next Week', "2 Weeks", "Next Month"];
+  const [isPickerVisible1, setPickerVisible1] = useState(false);
+  const [isPickerVisible2, setPickerVisible2] = useState(false);
+  const options1 = [
+    { label: 'Day',  value: 'Day' },
+    { label: 'Week',  value: 'Week' },
+    { label: '2 Weeks',  value: '2 Weeks' },
+    { label: 'Month',  value: 'Month' },
+     
+  ];
+  const options2 = [ 
+    { label: 'Today',  value: 'Today' },
+    { label: 'Next Week',  value: 'Next Week' },
+    { label: '2 Weeks',  value: '2 Weeks' },
+    { label: 'Next Month',  value: 'Next Month' },
+];
 
   return (
     <View style={styles.container}>
-       <View style={styles.row}>
+           <View style={styles.row}>
         <Text style={styles.label}>Every</Text>
-        <View style={styles.pickerContainer}>
-      <Picker
-        selectedValue={selectedOption1}
-        onValueChange={(itemValue) => setSelectedOption1(itemValue)}
-        style={styles.picker}>
-    
-        {options1.map((option, index) => (
-          <Picker.Item key={index} label={option} value={option} />
-        ))}
-      </Picker>
-      </View>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Starting</Text>
-        <View style={styles.pickerContainer}>
-      <Picker
-        selectedValue={selectedOption2}
-        onValueChange={(itemValue) => setSelectedOption2(itemValue)}
+        <TouchableOpacity onPress={() => setPickerVisible1(!isPickerVisible1)}>
+        <View style={styles.selectedContainer}>
+        <Text style={styles.selectedItemText}>{selectedItem1}</Text>
+        <Iconify icon="il:arrow-down" width={10}/>
+        </View>
+        </TouchableOpacity>
+
+        </View>
+
+      {isPickerVisible1 && (
+        <Picker
         style={styles.picker}
-      >
-        {options2.map((option, index) => (
-          <Picker.Item key={index} label={option} value={option} />
-        ))}
-      </Picker>
-      </View>
-      </View>
+          selectedValue={selectedItem1}
+          onValueChange={(value) => {
+            setSelectedItem1(value);
+            setPickerVisible1(false);
+          }}
+        >
+          <Picker.Item label={"Select An Option"} value={null} />
+          {options1.map((option) => (
+            <Picker.Item key={option.value} label={option.label} value={option.value} />
+          ))}
+        </Picker>
+      )}
+             <View style={styles.row}>
+        <Text style={styles.label}>Starting</Text>
+        <TouchableOpacity onPress={() => setPickerVisible2(!isPickerVisible2)}>
+        <View style={styles.selectedContainer}>
+        <Text style={styles.selectedItemText}>{selectedItem2}</Text>
+        <Iconify icon="il:arrow-down" width={10}/>
+        </View>
+        </TouchableOpacity>
+
+        </View>
+
+      {isPickerVisible2 && (
+        <Picker
+        style={styles.picker}
+          selectedValue={selectedItem2}
+          onValueChange={(value) => {
+            setSelectedItem2(value);
+            setPickerVisible2(false);
+          }}
+        >
+          <Picker.Item label="Select An Option" value={null} />
+          {options2.map((option) => (
+            <Picker.Item key={option.value} label={option.label} value={option.value} />
+          ))}
+        </Picker>
+      )}
     </View>
   );
 };
@@ -51,21 +88,36 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   label: {
-    fontSize: 14,
-    marginBottom: 8,
+    fontSize: 18,
+    marginBottom: 5,
+    padding: 10,
+    color: "#707070"
+  },
+  selectedContainer: {
+
+flexDirection: "row",
+paddingLeft: 10,
+alignContent: "center",
+gap: 5
   },
   picker: {
-    width: 200,
-    height: 40,
-
+    alignSelf: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 190,
+    borderBottomWidth: 1,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    border: "#ccc",
+    borderWidth: 1,
   },
   pickerContainer: {
     marginLeft: 5,
     justifyContent: "center",
     borderBottomColor: "#000",
-    borderBottomWidth: 1,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+
   },
   row: {
     flexDirection: "row",
@@ -73,7 +125,12 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 20,
   
-  }
+  },
+  selectedItemText: {
+    fontSize: 16,
+    fontWeight: "600",
+    
+  },
 });
 
 
