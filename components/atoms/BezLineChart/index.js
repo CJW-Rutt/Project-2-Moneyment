7eb9399ab2676ee75d68edab842f6ac151b54016
jnip2 <jnip2@my.bcit.ca>
2023-11-21 13:55:00 -0800

    add dark mode for chart labels

--- components/atoms/BezLineChart/index.js
+++ components/atoms/BezLineChart/index.js
@@ -4,10 +4,14 @@ import { StyleSheet } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
 import { Dimensions } from "react-native";
 import { Text } from 'react-native-paper';
+import { useContext } from 'react';
+import { DarkModeContext } from '../../../context/darkMode';
 
 const screenWidth = Dimensions.get("window").width;
 
 export default function BezLineChart() {
+    const { isDarkMode } = useContext(DarkModeContext);
+
     const data = {
         labels: ["Sep 4", "Sep 25", "Oct 2", "Oct 16"],
         datasets: [
@@ -19,13 +23,25 @@ export default function BezLineChart() {
         ],
     };
 
-    const chartConfig = {
+    const chartConfigLight = {
         backgroundGradientFrom: "#fff",
         backgroundGradientFromOpacity: 0,
         backgroundGradientTo: "#fff",
         backgroundGradientToOpacity: 0,
         color: (opacity = 1) => `rgba(66,148,136, ${opacity})`,
         labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional,
+    };
+
+    const chartConfigDark = {
+        backgroundGradientFrom: "#000",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#000",
+        backgroundGradientToOpacity: 0,
+        color: (opacity = 1) => `rgba(66,148,136, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(265, 265, 265, ${opacity})`,
         strokeWidth: 2, // optional, default 3
         barPercentage: 0.5,
         useShadowColorFromDataset: false // optional,
@@ -39,7 +55,7 @@ export default function BezLineChart() {
                 data={data}
                 width={screenWidth + 20}
                 height={180}
-                chartConfig={chartConfig}
+                chartConfig={isDarkMode ? chartConfigDark : chartConfigLight}
                 fromZero={true}
                 withHorizontalLines={false}
                 withVerticalLines={false}
@@ -49,72 +65,6 @@ export default function BezLineChart() {
         </>
 
     )
-
-
-
-    // const axesSvg = { fontSize: 12, fill: 'black' };
-    // const verticalContentInset = { top: 10, bottom: 0 }
-    // const xAxisHeight = 10
-    // const yAxisHeight = 10
-
-    // const keys = ['Sept 4', 'Sept 25', 'Oct 2', 'Oct 16']
-    // const data = num
-    // const latestData = data.length - 1
-    // const currentBalance = data[latestData]
-
-    // useEffect(() => {
-    //     console.log(latestData)
-    //     console.log(currentBalance)
-    // }, [])
-
-
-    // const CustomLine = ({ line }) => (
-    //     <Path
-    //         key="line"
-    //         d={line}
-    //         stroke="#6AB4AC"
-    //         strokeWidth={3}
-    //         fill="none"
-    //     />
-    // );
-
-    // return (
-    //     <View>
-    //         <Text style={styles.viewText}>{view}</Text>
-    //         <Text style={styles.balance}>${currentBalance.toLocaleString()}</Text>
-
-    //         <View style={{ height: 240, width: 400, padding: 30, flexDirection: 'row' }}>
-    //             <YAxis
-    //                 data={data}
-    //                 style={{ marginBottom: yAxisHeight }}
-    //                 contentInset={verticalContentInset}
-    //                 svg={axesSvg}
-    //                 numberOfTicks={data.length}
-
-    //             />
-    //             <View style={{ flex: 1, marginLeft: 10 }}>
-    //                 <AreaChart
-    //                     animate={true}
-    //                     animationDuration={350}
-    //                     style={{ flex: 1 }}
-    //                     data={data}
-    //                     contentInset={verticalContentInset}
-    //                     svg={{ fill: 'rgba(66, 148, 136, 0.1)' }}>
-    //                     <CustomLine />
-
-    //                 </AreaChart>
-    //                 <XAxis
-    //                     style={{ marginHorizontal: -10, height: xAxisHeight }}
-    //                     data={data}
-    //                     formatLabel={(value, index) => keys[value]}
-    //                     contentInset={{ left: 17, right: 17, }}
-    //                     svg={axesSvg}
-    //                 />
-    //             </View>
-    //         </View>
-    //     </View>
-
-    // )
 }
 
 const styles = StyleSheet.create({
