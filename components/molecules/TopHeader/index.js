5829d6ba5c289f7db56ecebbcbb2bc535a0b34c9
jnip2 <jnip2@my.bcit.ca>
2023-11-29 15:22:57 -0800

    adjust log in bar

--- components/molecules/TopHeader/index.js
+++ components/molecules/TopHeader/index.js
@@ -2,7 +2,9 @@ import { View, StyleSheet, Dimensions, Pressable } from "react-native"
 import { Text, Divider } from "react-native-paper"
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import { DarkModeContext } from "../../../context/darkMode";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import { auth } from "../../../firebase/firebase.config";
+import { onAuthStateChanged } from "firebase/auth";
 
 const screenWidth = Dimensions.get('screen').width;
 
@@ -12,9 +14,28 @@ export default function TopHeader({
     func
 }) {
     const { isDarkMode } = useContext(DarkModeContext)
+    const [signedIn, setSignedIn] = useState(false)
+
+    const checkUser = async () => {
+        await onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid;
+                console.log('signed in on TopHeader', uid)
+                setSignedIn(true)
+            } else {
+                setSignedIn(false)
+                console.log('not signed in')
+            }
+        })
+    }
+
+    useEffect(() => {
+        checkUser()
+    }, [])
+
     return (
         <>
-            <View style={styles.container}>
+            <View style={signedIn ? styles.container : styles.demoContainer}>
                 {
                     type === 'close'
                         ? <Pressable onPress={func} style={styles.icon}>
@@ -24,7 +45,7 @@ export default function TopHeader({
                         </Pressable>
                         : <></>
                 }
-                <Text variant="titleLarge" style={styles.text}>
+                <Text variant="titleLarge" style={signedIn ? styles.text : styles.demoText}>
                     {title}
                 </Text>
             </View>
@@ -46,10 +67,29 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         position: 'relative'
     },
+    demoContainer: {
+        minHeight: 90,
+        width: screenWidth,
+        maxHeight: 90,
+        flex: 1,
+        // justifyContent: 'space-between',
+        flexDirection: 'row',
+        borderBottomColor: '#A9A9A9',
+        borderBottomWidth: 1,
+        // alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+    },
     text: {
         textAlign: 'center',
         marginTop: 20,
         marginBottom: 10,
+        fontWeight: 800
+    },
+    demoText: {
+        textAlign: 'center',
+        marginTop: 55,
+        marginBottom: 5,
         fontWeight: 800
     },
     icon: {
